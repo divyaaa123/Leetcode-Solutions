f7e2ab4c90845f33ac6627f403b4aa2bdf27f533
Divya Jain <favoureddivya444@gmail.com>
2021-06-03 23:55:37 +0530

    Time: 68 ms (74.92%), Space: 32.1 MB (91.01%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,17 +1,18 @@
-
 class Solution {
 public:
-     const long long int mod = 1000000007;
+    const long long mod=1000000007;
+    int getMaxDiff(vector<int>& v, int len) {
+        sort(v.begin(), v.end());
+        int max_diff = v[0];
+        
+        for (int i = 1; i < v.size(); i++)
+            max_diff = max(max_diff, v[i]-v[i-1]);
+        
+        max_diff = max(max_diff, len - v.back());
+        return max_diff;
+    }
+    
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        sort(verticalCuts.begin(),verticalCuts.end());
-        sort(horizontalCuts.begin(),horizontalCuts.end());
-        int m1=max(horizontalCuts[0], h-horizontalCuts[horizontalCuts.size()-1]), m2=max(verticalCuts[0], w-verticalCuts[verticalCuts.size()-1]);
-        for(int i=1;i<horizontalCuts.size();i++){
-            m1=max(m1,horizontalCuts[i]-horizontalCuts[i-1]);
-        }
-        for(int i=1;i<verticalCuts.size();i++){
-            m2=max(m2,verticalCuts[i]-verticalCuts[i-1]);
-        }
-        return (m1%mod*m2%mod)%mod;
+        return (long)getMaxDiff(horizontalCuts, h) * getMaxDiff(verticalCuts, w) %mod;
     }
 };
