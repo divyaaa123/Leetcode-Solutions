778ff7db3c77a46f2c6e56925eb2d6b9dfc96d19
Divya Jain <favoureddivya444@gmail.com>
2021-06-23 00:06:46 +0530

    Time: 820 ms (61.79%), Space: 343.5 MB (25.41%) - LeetHub

--- minimum-absolute-difference-queries/minimum-absolute-difference-queries.cpp
+++ minimum-absolute-difference-queries/minimum-absolute-difference-queries.cpp
@@ -8,34 +8,29 @@ public:
         for(int i=1;i<=n;i++){
          for(int j=1;j<101;j++)
              v[i][j]=v[i-1][j];
-            v[i][nums[i-1]]+=1;
+            v[i][nums[i-1]]++;
         }
         for(int i=0;i<q;i++){
             int l=queries[i][0];
             int r=queries[i][1];
-            set<int> s;
                int m=INT_MAX;
             int a=0;
             for(int j=1;j<101;j++){
                 if((v[r+1][j]-v[l][j])!=0)
-                {  
-                   if(a==0)
-                   {
-                       a=j;
-                   }
-                    else
+                { 
+                    if(a!=0)
                     {
-                      //cout<<j<<" ";
                         m=min(m,j-a);
-                        a=j;
+                       
                     }
+                     a=j;
                 }
+                
             }
-     //  cout<<"\n";       
             if(m==INT_MAX)
                 ans.push_back(-1);
             else
-                          ans.push_back(m);
+                ans.push_back(m);
         }
         return ans;
     }
