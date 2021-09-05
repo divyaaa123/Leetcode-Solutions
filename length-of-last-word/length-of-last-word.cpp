7cd70420d62bd2182bae522137d465982e47549c
Divya Jain <favoureddivya444@gmail.com>
2021-09-05 14:48:46 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (50.04%) - LeetHub

---
+++ length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int c=0;
+        int a=0;
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==' ' && c==0){
+                continue;
+            }
+             else if(s[i]==' ' && c==1){
+                 break;
+             }
+            else if(s[i]!=' '){
+                a++;
+                c=1;
+            }
+           
+                
+        }
+        return a;
+    }
+};
