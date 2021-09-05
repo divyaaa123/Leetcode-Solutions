f79025b2d58dc8c02dbaf59ec84e12379f8478a6
Divya Jain <favoureddivya444@gmail.com>
2021-09-05 14:30:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.31%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        map<char,char> m;
+        m[')']='(';
+        m['}']='{';
+        m[']']='[';
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else
+            {
+                if(st.empty())
+                    return false;
+                char x=st.top();
+               
+                if( x!=m[s[i]])
+                    return false;
+                 st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
