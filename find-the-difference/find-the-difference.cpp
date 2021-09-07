1e92023f3d118550405eb31f1e6938f9d66c3ba1
Divya Jain <favoureddivya444@gmail.com>
2021-09-07 09:51:34 +0530

    Time: 4 ms (74.04%), Space: 6.5 MB (88.40%) - LeetHub

--- find-the-difference/find-the-difference.cpp
+++ find-the-difference/find-the-difference.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        multiset<char> st(s.begin(),s.end());
+         int arr[26];
+        memset(arr,0,sizeof(arr));
         for(int i=0;i<t.size();i++){
-            if(st.find(t[i])==st.end())
-                return t[i];
-            st.erase(st.find(t[i]));
+          arr[t[i]-'a']^=1;
         }
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']^=1;
+        }
+        for(int i=0;i<26;i++)
+            if(arr[i]==1)
+                return i+'a';
         return '0';
-        //Space Complexity: O(N)
+        //Space Complexity: O(1)
         //Time Complexity: O(N)
     }
 };
