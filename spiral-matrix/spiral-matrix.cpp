681fd2fb1cfb8cbbc62b589fc92abf1dc93c12f0
Divya Jain <favoureddivya444@gmail.com>
2021-09-12 13:45:20 +0530

    Time: 3 ms (12.19%), Space: 6.9 MB (29.75%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -4,27 +4,34 @@ public:
         vector<int> result;
         int row=matrix.size();
         int col=matrix[0].size();
-        int up=0,left=0,right=col-1,down=row-1;
-        while(result.size()<row*col){
-            for(int cols=left;cols<=right;cols++)
-                result.push_back(matrix[up][cols]);
-           for(int rows=up+1;rows<=down;rows++){
-               result.push_back(matrix[rows][right]);
-           } 
-            if(up!=down){
-                for(int cols=right-1;cols>=left;cols--){
-                    result.push_back(matrix[down][cols]);
-                }
+        int l=0,r=col-1;
+        int up=0, down=row-1;
+      while(result.size()<(row*col)){
+            for(int i=l;i<=r;i++){
+             
+                result.push_back(matrix[up][i]);
             }
-            if(left!=right){
-                for(int rows=down-1;rows>up;rows--){
-                    result.push_back(matrix[rows][left]);
-                }
-            }
-            left++;
-            right--;
-            up++;
-            down--;
+          for(int i=up+1;i<=down;i++){
+               
+              result.push_back(matrix[i][r]);
+          }
+          if(up!=down){
+          for(int i=r-1;i>=l;i--){
+             
+              result.push_back(matrix[down][i]);
+          }
+          }
+          if(l!=r){
+          for(int i=down-1;i>up;i--){
+               
+              result.push_back(matrix[i][l]);
+          }
+          }
+           up++;
+          down--;
+          l++;
+          r--;
+         
         }
         return result;
     }
