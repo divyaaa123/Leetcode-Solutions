a03406f71c1e18b110c000fbb308fc8a46c6a9ba
Divya Jain <favoureddivya444@gmail.com>
2021-09-20 20:43:49 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (64.68%) - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    vector<string> letterCombinations(string digits) {
+     vector<string> v;
+        map<char,string> m;
+    m['2']="abc";
+    m['3']="def";
+    m['4']="ghi";
+    m['5']="jkl";
+    m['6']="mno";
+    m['7']="pqrs";
+    m['8']="tuv";
+    m['9']="wxyz";
+      string ans="";
+        util(digits,0,ans,v, m);
+        return v;
+    }
+    void util(string &digits, int i, string ans, vector<string>& v,  map<char,string> &m){
+        if(i==digits.size())
+        {
+            if(ans!="")
+            v.push_back(ans);
+            ans="";
+            return;
+        }
+       
+            for(int k=0;k<m[digits[i]].size();k++){
+                
+                util(digits,i+1,ans+m[digits[i]][k],v,m);
+            }
+    
+        
+    }
+};
