4f398af8eb9f12fca296d4ae38582c0a06288633
Divya Jain <favoureddivya444@gmail.com>
2021-10-28 09:14:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (77.16%) - LeetHub

---
+++ remove-palindromic-subsequences/remove-palindromic-subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int n=s.size();
+        int c=1;
+        for(int i=0;i<n/2;i++){
+            if(s[i]==s[n-i-1]){
+                c=1;
+            }
+            else
+            {
+                c=0;
+                break;
+            }
+        }
+        if(c==1)
+            return 1;
+        return 2;
+    }
+};
